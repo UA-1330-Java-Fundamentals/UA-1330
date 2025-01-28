f108bb7a5a54a7987457e880945f4bf89f407928
tenderbussiness <andriyvalyaev@gmail.com>
2025-01-28 23:57:53 +0200

    HW1 - done. test commit

---
+++ src/com/softserve/edu/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        float radius;
+        float area;
+        float perimeter;
+
+        System.out.println("Enter the radius of the circle: ");
+        Scanner scanner = new Scanner(System.in);
+        radius = scanner.nextInt();
+        area = radius * radius;
+        perimeter = 2 * radius * radius;
+        System.out.println("The area of the circle is " + area);
+        System.out.println("The perimeter of the circle is " + perimeter);
+        //commit
+
+    }
+}
