fbef05115d3194ebc081105fbe3f26fbc1570e6d
Daryna Tretiak <daryska2012@gmail.com>
2025-02-03 17:41:35 +0200

    add the first homework task of the hw3

---
+++ src/com/softserve/edu/hw3/homework/AreaCalculator.java
@@ -0,0 +1,25 @@
+package com.softserve.edu.hw3.homework;
+
+import java.util.Scanner;
+
+public class AreaCalculator {
+    public static void main (String[] args){
+        int firstSide = getSide ("Input side 1: ");
+        int secondSide = getSide ("Input side 2: ");
+        int thirdSide = getSide ("Input side 3: ");
+        System.out.print("the area of the triangle is %.2f".formatted(getArea(firstSide, secondSide,thirdSide)));
+    }
+
+    private static int getSide(String prompt){
+        Scanner scanner = new Scanner (System.in);
+        System.out.print(prompt);
+        return scanner.nextInt();
+    }
+
+    private static double getArea (int firstSide, int secondSide, int thirdSide){
+        double perimeter = (firstSide+secondSide+thirdSide) /2.0;
+        return Math.sqrt(perimeter* (perimeter-firstSide)*(perimeter-secondSide)*(perimeter-thirdSide));
+
+    }
+
+}
