3e2d8120bef4b3394a78a25b18300d9c92229090
Vlad <vladoverko1@gmail.com>
2025-02-03 19:45:51 +0300

    Branch created

--- src/com/softserve/edu/Appl.java
+++ src/com/softserve/edu/Appl.java
@@ -5,6 +5,8 @@ public class Appl {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.print("Start");
+        System.out.print("Start2");
+
     }
 
 }
