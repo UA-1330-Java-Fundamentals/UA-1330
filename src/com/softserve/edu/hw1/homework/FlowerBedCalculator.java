3e76b5c7eada485fd3fa13993333bdcb3cd2d5b9
oshuty <olya.shutileva@gmail.com>
2025-02-03 20:38:36 +0200

    some improvements

--- src/com/softserve/edu/hw1/homework/FlowerBedCalculator.java
+++ src/com/softserve/edu/hw1/homework/FlowerBedCalculator.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class FlowerBedCalculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Введіть радіус");
+        System.out.println("Введіть радіус");//Be careful with the language of the comments, it should be in English
         int radius = sc.nextInt();
 
         double perimeter = 2 * Math.PI * radius;
@@ -12,5 +12,6 @@ public class FlowerBedCalculator {
 
         System.out.println("Perimeter is " + perimeter);
         System.out.print("Area is " + area);
+        sc.close();//Don't forget to close the scanner
     }
 }
--- src/com/softserve/edu/hw3/homework/AreaCalculator.java
+++ src/com/softserve/edu/hw3/homework/AreaCalculator.java
@@ -3,23 +3,30 @@ package com.softserve.edu.hw3.homework;
 import java.util.Scanner;
 
 public class AreaCalculator {
-    public static void main (String[] args){
-        int firstSide = getSide ("Input side 1: ");
-        int secondSide = getSide ("Input side 2: ");
-        int thirdSide = getSide ("Input side 3: ");
-        System.out.printf("the area of the triangle is %.2f", getArea(firstSide, secondSide,thirdSide));
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int firstSide = getSide("Input side 1: ", scanner);
+        int secondSide = getSide("Input side 2: ", scanner);
+        int thirdSide = getSide("Input side 3: ", scanner);
+        System.out.printf("the area of the triangle is %.2f", getArea(firstSide, secondSide, thirdSide));
+        scanner.close();
     }
 
-    private static int getSide(String prompt){
-        Scanner scanner = new Scanner (System.in);
+    private static int getSide(String prompt, Scanner scanner) {// The name of the method should be more descriptive like readSideFromInput
+        //Scanner scanner = new Scanner (System.in);//Don't create an instance of Scanner in every method, create it once and use it in all methods
         System.out.print(prompt);
         return scanner.nextInt();
     }
 
-    private static double getArea (int firstSide, int secondSide, int thirdSide){
-        double perimeter = (firstSide+secondSide+thirdSide) /2.0;
-        return Math.sqrt(perimeter* (perimeter-firstSide)*(perimeter-secondSide)*(perimeter-thirdSide));
-
+    public static double getArea(int firstSide, int secondSide, int thirdSide) {
+        if (!isTriangle(firstSide, secondSide, thirdSide)) {
+            System.out.println("The sides do not form a triangle");
+        }
+        double halfPerimeter = (firstSide + secondSide + thirdSide) / 2.0;
+        return Math.sqrt(halfPerimeter * (halfPerimeter - firstSide) * (halfPerimeter - secondSide) * (halfPerimeter - thirdSide));
     }
-
+//some improvements
+    private static boolean isTriangle(int firstSide, int secondSide, int thirdSide) {
+        return firstSide + secondSide > thirdSide && firstSide + thirdSide > secondSide && secondSide + thirdSide > firstSide;
+    }
 }
