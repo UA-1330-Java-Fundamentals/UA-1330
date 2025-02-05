c251c95d148e9e1885c33f3b440300a8d6e5bbd4
oshuty <olya.shutileva@gmail.com>
2025-02-05 18:36:29 +0200

    comments to hw4

--- src/com/softserve/conditionals/HomeTask1.java
+++ src/com/softserve/conditionals/HomeTask1.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 
 public class HomeTask1 {
     public static void main(String[] args)  throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));//Scanner sc = new Scanner(System.in);
         System.out.println("Input the first number");
         int first = Integer.parseInt(br.readLine());
 
@@ -31,5 +31,48 @@ public class HomeTask1 {
                     " is not in range[-5, 5]");
         }
     }
+}
 
+//Check range for numbers [-5, 5] better to do with methods
+class CheckNumbersInRange {
+
+    static final int LEFT_BOUND = -5;
+    static final int RIGHT_BOUND = 5;
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args)  throws IOException {
+        System.out.println("Input the first number: ");
+        int first = Integer.parseInt(br.readLine());
+
+        System.out.println("Input the second number: ");
+        int second = Integer.parseInt(br.readLine());
+
+        System.out.println("Input the third number: ");
+        int third = Integer.parseInt(br.readLine());
+
+        checkNumbers(first, second, third);
+
+    }
+
+    /**
+     * Check if number is in range
+     * @param number number to check
+     * @return true if number is in range
+     */
+    public static boolean isInRange(int number) {
+        return number >= LEFT_BOUND && number <= RIGHT_BOUND;
+    }
+    /**
+     *  Check if all three numbers are in range
+     * @param first first number
+     * @param second second number
+     * @param third third number
+     */
+    public static void checkNumbers(int first, int second, int third) {
+        if (isInRange(first) && isInRange(second) && isInRange(third)) {
+            System.out.println("All three entered numbers " + first + ", " + second + ", and " + third + " are in range [-5, 5]");
+        } else {
+            System.out.println("Some of the entered numbers " + first + ", " + second + ", and " + third + " are not in range [-5, 5]");
+        }
+    }
 }
--- src/com/softserve/conditionals/HomeTask2.java
+++ src/com/softserve/conditionals/HomeTask2.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class HomeTask2 {
+public class HomeTask2 {//Naming is not correct, better FindMaxNumber
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Input the first number");
@@ -29,5 +29,18 @@ public class HomeTask2 {
         System.out.println("The maximum number of three entered numbers "
                     + first  + " , " + second + " , and " +
                     " is : " + maxNumber);
+        System.out.println("Max is: " + maxNumber(first, second, third));
+    }
+
+    /**
+     * Find the maximum number from three numbers
+     * @param a first number
+     * @param b second number
+     * @param c third number
+     * @return the maximum number
+     */
+    public static int maxNumber(int a, int b, int c) {
+        return Math.max(a, Math.max(b, c));
     }
 }
+//Everything is correct but try to use methods for solving tasks, it will be more readable and understandable
