9ce3a64b75279dad46113f5a454c90a108f60b46
oshuty <olya.shutileva@gmail.com>
2025-02-05 17:08:12 +0200

    some improvements

--- src/main/java/com/softserve/edu/Appl.java
+++ src/main/java/com/softserve/edu/Appl.java
@@ -1,17 +1,8 @@
-package java.com.softserve.edu;
+package com.softserve.edu;
 
 public class Appl {
     public static void main(String[] args) {
-        boolean bool = 2 > 5;
-        if (bool == true) {
-            System.out.print("a");
-        } else if (!bool) {
-            System.out.print("b");
-        }
-        if (bool == false) {
-            System.out.print("c");
-        } else {
-            System.out.print("d");
-        }
+        System.out.println("Hello world!");
     }
+
 }
