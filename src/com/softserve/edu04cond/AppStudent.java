bdef893775defae2d8bb6de1c05b17efdbac4aa0
Yaroslav <garasym@gmail.com>
2025-02-05 18:34:15 +0200

    Equals. HashCode

---
+++ src/com/softserve/edu04cond/AppStudent.java
@@ -0,0 +1,18 @@
+package com.softserve.edu04cond;
+
+public class AppStudent {
+
+    public static void main(String[] args) {
+        Student st1 = new Student("Ira", 25);
+        Student st2 = new Student("Ira", 25);
+        //
+        System.out.println("st1 = " + st1); // st1.toString()
+        System.out.println("st2 = " + st2.toString()); // st2.toString()
+        //
+        System.out.println("st1 == st2 : " + (st1 == st2));
+        System.out.println("st1.equals(st2) : " + st1.equals(st2));
+        //
+        System.out.println("st1.hashCode() : " + st1.hashCode());
+        System.out.println("st2.hashCode() : " + st2.hashCode());
+    }
+}
