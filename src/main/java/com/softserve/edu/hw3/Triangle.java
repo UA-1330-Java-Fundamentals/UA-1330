641a67ebd408c048efe98b5cf16b93500a9daa20
Nataliia Zinchenko <Nataliia Zinchenko>
2025-02-08 13:02:45 +0000

    refactoring to make method getArea()

--- src/main/java/com/softserve/edu/hw3/Triangle.java
+++ src/main/java/com/softserve/edu/hw3/Triangle.java
@@ -2,6 +2,13 @@ package com.softserve.edu.hw3;
 import java.util.Scanner;
 
 public class Triangle {
+
+    public static double getArea(double s1, double s2, double s3){
+        double semiper = (s1 + s2 + s3) / 2;
+        double area = Math.sqrt(semiper * (semiper - s1) * (semiper - s2) * (semiper - s3));
+        return area;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the 1st side of triangle: ");
@@ -10,8 +17,9 @@ public class Triangle {
         double s2 = sc.nextFloat();
         System.out.println("Enter the 3rd side of triangle: ");
         double s3 = sc.nextFloat();
-        double semiper = (s1 + s2 + s3) / 2;
-        double area = Math.sqrt(semiper * (semiper - s1) * (semiper - s2) * (semiper - s3));
-        System.out.printf("The area of the triange is: %.2f", area);
+        /* double semiper = (s1 + s2 + s3) / 2;*/ 
+        /* double area = Math.sqrt(semiper * (semiper - s1) * (semiper - s2) * (semiper - s3)); */
+        System.out.printf("The area of the triange is: %.2f", getArea(s1, s2, s3)); 
     }
 }
+
