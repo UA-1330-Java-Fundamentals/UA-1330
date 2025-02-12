407c354aa9e56af4ac8b122d2db451d602953df5
Yaroslav <garasym@gmail.com>
2025-02-12 20:23:16 +0200

    Inheritance. Examples. Updated

--- src/com/softserve/hw08/AppBird.java
+++ src/com/softserve/hw08/AppBird.java
@@ -3,10 +3,15 @@ package com.softserve.hw08;
 public class AppBird {
 
     public static void main(String[] args) {
-        Bird[] birds = { new Eagle(), new Swallow(), new Penguin(), new Kiwi() };
+        //Bird[] birds = new Bird[] { new Eagle(), new Swallow(), new Penguin(), new Kiwi() };
+        Bird[] birds = {new Eagle(), new Swallow(), new Penguin(), new Kiwi()};
         //
         for (Bird currentBird : birds) {
-            System.out.println("currentBird = " + currentBird);
+            System.out.println("currentBird = " + currentBird); // currentBird.toString();
+        }
+        //
+        for (int i = 0; i < birds.length; i++) {
+            System.out.println("\tcurrent  = " + birds[i]);
         }
     }
 }
