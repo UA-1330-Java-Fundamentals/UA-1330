9225e4018a3579cfeb792234a9fe27ab6ce1e055
Sergii <variant11111@gmail.com>
2025-02-16 02:55:13 +0200

    task convertTemperature

---
+++ src/com/softserve/edu/hw3oop/TemperatureConverter.java
@@ -0,0 +1,15 @@
+package com.softserve.edu.hw3oop;
+
+import static java.lang.System.*;
+
+public class TemperatureConverter {
+
+    public static void main(String[] args) {
+
+        double FT = 100.1;
+        double CT = (FT -32) / 1.8;
+
+        out.println(CT);
+
+    }
+}
