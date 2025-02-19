dc91cbdb9ee59bf14764bcd57ab551918057ca3c
Karyna Shevchenko <shevchenkoks1810@gmail.com>
2025-02-19 13:49:58 +0200

    fixes to homework 6

--- src/homework6/task3/PreferredCustomer.java
+++ src/homework6/task3/PreferredCustomer.java
@@ -11,7 +11,10 @@ public class PreferredCustomer extends Customer{
 
     //Applies the discount to the purchase amount and returns the discounted amount (type: int).
     public int applyDiscount(int amount){
-        return (int)(amount * (1 - discountRate)); //(amount - (amount * discountRate));
+        return (int) (amount - (amount * discountRate / 100));
+
+
+
     }
 
     //Overrides the addPurchase method from the Customer class, adding the purchase with a discount to the purchaseHistory array.
@@ -20,6 +23,7 @@ public class PreferredCustomer extends Customer{
         if ( purchaseCount < purchaseHistory.length){
             purchaseHistory[purchaseCount] = applyDiscount(amount);
             purchaseCount++;
+            System.out.println("Adding purchase with discount: Original: $" + amount + ", After Discount: $" + applyDiscount(amount));
         } else {
             System.out.println("The array is full.");
         }
