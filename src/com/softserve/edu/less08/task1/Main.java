9864ffccfc01383791f3db5fcedd0830d4ad6818
Mariia Malyhina <mariia.malyhina@monobank.ua>
2025-03-09 23:29:07 +0200

    fix

--- src/com/softserve/edu/less08/task1/Main.java
+++ src/com/softserve/edu/less08/task1/Main.java
@@ -1,17 +1,5 @@
 package com.softserve.edu.less08.task1;
 
-import java.util.ArrayList;
-import java.util.List;
-
-/*
-o переопределите метод activity() из класса Person. Метод activity() должен
-возвращать строковое значение, которое является типом активности для соответствующего подтипа Person,
-например, для студента - это может быть значение "Я учусь в университете".
-• В методе main() создайте два экземпляра класса Student и выведите информацию
-о них, вызвав соответствующие методы info() и activity().
-• Создайте еще один экземпляр класса Student, клонировав первого студента, измените курс
-для этого объекта и выведите полную информацию о созданном студента
- */
 public class Main {
     public static void main(String[] args) throws CloneNotSupportedException {
        Student student1 = new Student(new Person.FullName("Mariia","Malyhina"),28,5);
