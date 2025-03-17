36e9b6358406e357e57501d8f472a7af8de5c994
Daryna Tretiak <daryska2012@gmail.com>
2025-03-17 13:17:36 +0200

    add first part of hw9

--- src/com/softserve/edu/hw9/Collection.java
+++ src/com/softserve/edu/hw9/Collection.java
@@ -1,9 +1,6 @@
 package com.softserve.edu.hw9;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Collections;
+import java.util.*;
 
 public class Collection {
     public static void main(String[] args) {
@@ -14,27 +11,27 @@ public class Collection {
         }
         System.out.println("Original list " + myCollection);
 
-       int max = myCollection.indexOf(Collections.max(myCollection));
-       int min = myCollection.indexOf(Collections.min(myCollection));
-       Collections.swap(myCollection,max, min);
-       System.out.println("list after swapping min and max values " + myCollection);
+        int max = myCollection.indexOf(Collections.max(myCollection));
+        int min = myCollection.indexOf(Collections.min(myCollection));
+        Collections.swap(myCollection,max, min);
+        System.out.println("list after swapping min and max values " + myCollection);
 
-       int randomNumber = random.nextInt(900) + 100;
-       for (int i = 0; i < myCollection.size(); i++) {
-           if (myCollection.get(i) < 0) {
-               myCollection.add(i, randomNumber);
-               break;
-           }
-       }
-        System.out.println("list after adding random number" + myCollection);
+        int randomNumber = random.nextInt(900) + 100;
+        for (int i = 0; i < myCollection.size(); i++) {
+            if (myCollection.get(i) < 0) {
+                myCollection.add(i, randomNumber);
+                break;
+            }
+        }
+        System.out.println("list after adding random number " + myCollection);
 
-       for (int i = 0; i < myCollection.size() -1; i++) {
-           if (myCollection.get(i) < 0 && myCollection.get(i +1) > 0 || myCollection.get(i) > 0 && myCollection.get(i +1) < 0) {
-               myCollection.add(i+1, 0);
-               i++;
-               System.out.println(myCollection.size());
-           }
-       }
+        for (int i = 0; i < myCollection.size() -1; i++) {
+            if (myCollection.get(i) < 0 && myCollection.get(i +1) > 0 || myCollection.get(i) > 0 && myCollection.get(i +1) < 0) {
+                myCollection.add(i+1, 0);
+                i++;
+            }
+        }
         System.out.println("list after adding 0 " + myCollection);
     }
 }
+
