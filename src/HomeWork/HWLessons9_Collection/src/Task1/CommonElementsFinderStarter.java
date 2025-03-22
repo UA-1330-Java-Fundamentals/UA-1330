9664d1a254df1f19fb6a178ce448d08cf594da41
alona <zahychenko.alona8@gmail.com>
2025-03-22 14:34:25 +0200

    HW Lessons9 Collection add task 1

---
+++ src/HomeWork/HWLessons9_Collection/src/Task1/CommonElementsFinderStarter.java
@@ -0,0 +1,33 @@
+package Task1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class CommonElementsFinderStarter {
+
+    public static List<Integer> findCommonElements(List<Integer> list1, List<Integer> list2) {
+        List<Integer> commonElements = new ArrayList<>();
+        if (list1 == null || list2 == null || list1.isEmpty() || list2.isEmpty()){
+            //return new ArrayList<>();
+            return commonElements;
+        }
+        //List<Integer> commonElements = new ArrayList<>(list1);
+        for (Integer element : list1) {
+            if (element != null && list2.contains(element) && !commonElements.contains(element)) {
+                commonElements.add(element);
+            }
+        }
+        //commonElements.retainAll(list2);
+        return new ArrayList<>(commonElements);
+    }
+
+    public static Integer findMaxValue(List<Integer> commonElements) {
+        if (commonElements == null || commonElements.isEmpty()) {
+            return null;
+        }
+
+        int maxValue = Collections.max(commonElements);
+        return maxValue;
+    }
+}
---
+++ src/HomeWork/HWLessons9_Collection/src/Task1/ElementsAppl.java
@@ -0,0 +1,31 @@
+package Task1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ElementsAppl {
+    public static void main(String[] args) {
+        //List<Integer> list1 = new ArrayList<>(List.of(5, 5, 5));
+        List<Integer> list1 = List.of(5, 5, 5);
+        //List<Integer> list1 = List.of();
+        List<Integer> list2 = List.of(2, 3, 4, 5, 9);
+
+
+        List<Integer> commonElements = CommonElementsFinderStarter.findCommonElements(list1, list2);
+        if (commonElements == null || commonElements.isEmpty()) {
+            System.out.println("No common elements");
+        } else {
+            System.out.println("Common elements: " + commonElements);
+        }
+
+        Integer maxValue = CommonElementsFinderStarter.findMaxValue(commonElements);
+        if (maxValue != null) {
+            System.out.println("Maximum value among common elements: " + maxValue);
+        } else {
+            System.out.println("No common elements to determine the maximum value " + maxValue);
+        }
+    }
+
+
+}
+
