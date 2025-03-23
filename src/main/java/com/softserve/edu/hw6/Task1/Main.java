baadea1cafd98e6807e221ab9ef197072f94b978
Nataliia Zinchenko <Nataliia Zinchenko>
2025-03-23 15:43:12 +0000

    Task 2

--- src/main/java/com/softserve/edu/hw6/Task1/Main.java
+++ src/main/java/com/softserve/edu/hw6/Task1/Main.java
@@ -1,6 +1,4 @@
 package com.softserve.edu.hw6.Task1;
-
-public class Main {
     public static void main(String[] args) {
         Bird[] birds = new Bird[4];
 
@@ -17,4 +15,3 @@ public class Main {
             System.out.println("Feathers: " + bird.feathers + ", Lay Eggs: " + bird.layEggs);
         }
     }
-}
